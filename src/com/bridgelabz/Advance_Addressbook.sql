4ab05397e5712409a2525c44e7272be9bf0aaa1c
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-10 00:42:38 +0530

    [Kavyasree J Ganesh] Addressbook sql problem

---
+++ src/com/bridgelabz/Advance_Addressbook.sql
@@ -0,0 +1,20 @@
+---Use Case 1---
+---Ability to create a Address Book Service DB---
+
+mysql>create database addressbook_service;
+1 row(s) affected
+
+mysql>show databases
+/*addressbook_service
+information_schema
+mysql
+payroll_service
+performance_schema
+sakila
+sys
+world */
+
+mysql>use
+Database changed
+
+
