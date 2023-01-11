e1cc06f7f0f2cd77a0ab9257cf11fad8ebf89b13
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-11 21:29:07 +0530

    [Kavyasree J ganesh] Addrssbook sql

--- src/com/bridgelabz/Advance_Addressbook.sql
+++ src/com/bridgelabz/Advance_Addressbook.sql
@@ -5,7 +5,8 @@ mysql>create database addressbook_service;
 1 row(s) affected
 
 mysql>show databases
-/*addressbook_service
+/*
+addressbook_service
 information_schema
 mysql
 payroll_service
@@ -16,5 +17,38 @@ world */
 
 mysql>use
 Database changed
+
+
+---Use Case 2---
+---Ability to create a Address Book Table---
+mysql> create table addressbook(
+       id int not null auto_increment,
+       first_name varchar(30) not null ,
+       last_name varchar(30) not null ,
+       address varchar(30) not null ,
+       city varchar(30) not null ,
+       state varchar(30) not null ,
+       zip varchar(30) not null ,
+       phone_number varchar(15) not null ,
+       email varchar(30) not null ,
+       primary key(id)
+       );
+
+---0 row(s) affected ---
+mysql> desc addressbook
+
+/*
+Field	Type	Null	Key	Default	Extra
+id	int	NO	PRI		auto_increment
+first_name	varchar(30)	NO
+last_name	varchar(30)	NO
+address	varchar(30)	NO
+city	varchar(30)	NO
+state	varchar(30)	NO
+zip	varchar(30)	NO
+phone_number	varchar(15)	NO
+email	varchar(30)	NO
+*/
+
 
 
